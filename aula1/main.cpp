ad505caddd049ffa7c829b9f66b9b1d195419a7c
jpachec0 <bcreeper825@gmail.com>
2026-03-19 12:01:18 -0300

    Atualização na aula 1

--- aula1/main.cpp
+++ aula1/main.cpp
@@ -4,7 +4,12 @@
 #include <string.h>
 using namespace std;
 
+//Função principal do programa, onde a execução começa
 int main(){
+    //Cout serve para imprimir algo na tela, e endl serve para pular uma linha
+    //O << é o operador de inserção, que insere o que está à direita dele na saída padrão (tela)
     cout<< "Hello, world" <<endl;
-    return 0;
+    //Pode-se usar o \n para pular uma linha, mas o endl é mais recomendado pois ele também limpa o buffer de saída
+    cout<< "\nHello, world";
+    return 0; //Retorna 0 para indicar que o programa terminou com sucesso
 }
