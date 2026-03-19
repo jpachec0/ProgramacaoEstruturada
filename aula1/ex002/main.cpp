f3c8a8259a0ee28080a226aecb27f2c4fa12a092
jpachec0 <bcreeper825@gmail.com>
2026-03-19 14:06:57 -0300

    Exercício 2 realizado

---
+++ aula1/ex002/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout << "Digite o primeiro numero: ";
+    cin >> num1;
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+
+    cout << "O número 1 é: " << num1 << endl;
+    cout << "O número 2 é: " << num2 << endl;
+
+    return 0;
+}
