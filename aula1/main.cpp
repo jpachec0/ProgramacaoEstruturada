b4260b1e10dcf1223cc681cf8c9379dfeb3c1ec7
jpachec0 <bcreeper825@gmail.com>
2026-03-19 11:31:54 -0300

    Primeiro projeto em C++

---
+++ aula1/main.cpp
@@ -0,0 +1,10 @@
+//Vamos inserir nas proximas linhas algumas bibliotecas
+/*As bibliotecas são arquivos que contêm código pré-escrito que podemos utilizar em nosso programa*/
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    cout<< "Hello, world" <<endl;
+    return 0;
+}
